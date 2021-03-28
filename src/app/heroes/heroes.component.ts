4038b0006510000de5f8eb2eddc31710d97c0742
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-28 16:47:03 +0900

    feat: heroを選択した履歴を表示するようにしました

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -23,6 +27,13 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    if (!this.isSameMessage(`HeroesComponent: Selected hero id=${hero.id}`)) {
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    }
+  }
+
+  isSameMessage(message: string): boolean {
+    return this.messageService.messages.some((msg) => msg === message);
   }
 
 }
