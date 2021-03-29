ebcf312d8da53c891d14ff11686ae3d187a0aab0
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-29 22:03:21 +0900

    feat: heroes component を修正しました

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,9 +24,4 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 }
