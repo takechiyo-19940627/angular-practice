1c3057d08495e455c461c4872366db6aebd1fd6e
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-29 21:50:55 +0900

    feat: hero-detailへのroutingを追加しました

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes = [
   { path: '', component: DashboardComponent },
-  { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent },
 ]
 
 @NgModule({
