9db3f50b13785fe395bbf0d60e5855c51e66313a
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-29 22:29:50 +0900

    feat: hero-detailに戻るボタンを設置しました

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,6 +26,9 @@ export class HeroDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
