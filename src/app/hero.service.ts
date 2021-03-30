b2febc1510c362d08e3c1aaac4fb65ef93118ad1
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-30 23:17:00 +0900

    feat: hero をhttp通信で取得するようにしました

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,13 +20,17 @@ export class HeroService {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
         tap(heroes => this.log('fetched heroes')),
-        catchError(this.handleError('getHeroes', []))
+        catchError(this.handleError('getHeroes', [])),
       );
   }
 
   getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`)),
+    );
   }
 
   private log(message: string):void {
