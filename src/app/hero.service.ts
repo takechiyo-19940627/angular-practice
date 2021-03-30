743cd0c1d9c6dfd6335e9f4974dcf44e5a6ce9c8
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-03-30 23:02:44 +0900

    feat: http通信でheroesを取得するようにしました

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
@@ -8,16 +9,24 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     this.messageService.add('HeroService:fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  private log(message: string):void {
+    this.messageService.add(`HeroService:${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
 }
