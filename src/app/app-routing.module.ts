8bd14fbedcdc67b0996722a934db7d4c87f0d7f8
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-04 00:10:26 +0900

    feat: crisis list  ページへのルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,6 +10,7 @@ const routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'crisis-list', component: CrisisListComponent },
 ]
 
 @NgModule({
