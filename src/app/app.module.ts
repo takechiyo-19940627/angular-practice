1bab0495f3d935cc033cccb2f217cae6ad16b6f7
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-04 13:31:25 +0900

    feat: heroModuleを追加しました

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { HeroListComponent } from './hero-list/hero-list.component';
       InMemoryDataService,
       { dataEncapsulation: false }
     ),
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
