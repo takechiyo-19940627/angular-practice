d21c53f18eb6a6854f032b32018c2c4fa152af7e
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-04 14:51:19 +0900

    feat: crisis-center componentを追加しました

--- src/app/crisis-center/crisis-center.module.ts
+++ src/app/crisis-center/crisis-center.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CrisisCenterComponent],
   imports: [
     CommonModule,
     CrisisCenterRoutingModule
