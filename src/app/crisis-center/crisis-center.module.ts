0472762f4e15421c24fd3ea6678eb2f0b6cff480
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-04 14:59:10 +0900

    feat: CrisisDetailComponentを追加しました

--- src/app/crisis-center/crisis-center.module.ts
+++ src/app/crisis-center/crisis-center.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CrisisCenterRoutingModule } from './crisis-center-routing.module';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 
 
 @NgModule({
-  declarations: [CrisisCenterComponent, CrisisCenterHomeComponent],
+  declarations: [CrisisCenterComponent, CrisisCenterHomeComponent, CrisisDetailComponent],
   imports: [
     CommonModule,
     CrisisCenterRoutingModule
