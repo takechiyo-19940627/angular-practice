74c3d7696e70331cf4cfa07bfb1f6efbd07dffbc
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-04 13:12:48 +0900

    feat: NotFoundPageを追加しました

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'crisis-list', component: CrisisListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ]
 
 @NgModule({
