1710744ed07eae577c09e511a08f3886f16cabf1
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2021-04-18 17:16:33 +0900

    feat: login, logout機能をLoginComponentに実装

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-    console.log('AuthGuard#canActivate called');
-    return true;
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): true|UrlTree {
+    const url: string = state.url;
+
+    return this.checkLogin(url);
   }
   
+  checkLogin(url: string): true|UrlTree {
+    if (this.authService.isLoggedIn) { return true; }
+
+    this.authService.redurectUrl = url;
+
+    return this.router.parseUrl('/login');
+  }
 }
